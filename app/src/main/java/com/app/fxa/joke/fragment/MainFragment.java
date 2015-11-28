5bf2d9ad21b48570589b32425350aedf3da016eb
303153041@qq.com <fengxiang1990529>
2015-11-28 18:59:23 +0800

    界面修改

--- app/src/main/java/com/app/fxa/joke/fragment/MainFragment.java
+++ app/src/main/java/com/app/fxa/joke/fragment/MainFragment.java
@@ -278,19 +278,19 @@ public class MainFragment extends BaseFragment implements SwipeRefreshLayout.OnR
         // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
         oks.setTitle(getString(R.string.share));
         // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-        oks.setTitleUrl("http://sharesdk.cn");
+        //oks.setTitleUrl("http://sharesdk.cn");
         // text是分享文本，所有平台都需要这个字段
         oks.setText(content);
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
         //   oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
         // url仅在微信（包括好友和朋友圈）中使用
-        oks.setUrl("http://sharesdk.cn");
+        //oks.setUrl("http://sharesdk.cn");
         // comment是我对这条分享的评论，仅在人人网和QQ空间使用
         oks.setComment("");
         // site是分享此内容的网站名称，仅在QQ空间使用
         oks.setSite(getString(R.string.app_name));
         // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-        oks.setSiteUrl("http://sharesdk.cn");
+        //oks.setSiteUrl("http://sharesdk.cn");
 
 // 启动分享GUI
         oks.show(getActivity());
